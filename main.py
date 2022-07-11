89801298625bdd6ebfaeb267c7fa54a3558e1765
Eriko <kipkoechsomoe@gmail.com>
2022-07-11 16:39:55 +0300

    Change sqlite to Postgres

--- main.py
+++ main.py
@@ -9,18 +9,19 @@ from flask_ckeditor import CKEditor
 from forms import CreatePostForm, CreateLoginForm, CreateRegisterForm, CreateCommentForm
 from datetime import date
 from functools import wraps
+import os
 
 # Delete this code:
 # import requests
 # posts = requests.get("https://api.npoint.io/43644ec4f0013682fc0d").json()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
